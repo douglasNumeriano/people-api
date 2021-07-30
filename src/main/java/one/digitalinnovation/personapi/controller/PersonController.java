52cc772946b4ecefd76a4b2af52aeb8f8bda6d8a
numeriano <douglas_rnmeriano@hotmail.com>
2021-07-30 10:15:28 -0300

    Correção do maven

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -38,7 +38,7 @@ public class PersonController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+    public void deleteByID(@PathVariable Long id) throws PersonNotFoundException {
         personService.delete(id);
     }
 
